db31ef4be42a252a0e20ae40175fa4917c80c193
HiddenJem245 <34327018+HiddenJem245@users.noreply.github.com>
2018-03-21 23:11:36 +0000

    add readline to main

--- shell.c
+++ shell.c
@@ -5,15 +5,34 @@
  * @argv: array of arguments
  * Return: Always 0
  */
-int main(__attribute__((unused))int argc, __attribute__((unused))char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
-	char *line = NULL;
+	pid_t pid;
+	char *buf;
+	size_t size;
+	int status;
 
-	while (1)
-	{
+	buf = malloc(size * sizeof(char));
+	if (buf == NULL)
+		return (-1);
 		printprompt("$ ");
-		line = read_line();
-		free(line);
-	}
+	while ((getline(&buf, &size, stdin)) != EOF)
+	{
+		pid = fork();
+		argv[0] = buf;
+		buf[_strlen(buf) - 1] = '\0';
+		if (pid < 0)
+		{
+			perror("Error:\n");
+			return (1);
+		}
+		if (pid == 0)
+		{
+			status = execve(argv[0], argv, NULL);
+		}
+		wait(&status);
+		printprompt("$ ");
+		}
+	free(buf);
 	return (0);
 }
